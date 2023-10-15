887a7d769ad54213596d0fd12d03f712396b2612
LostNativ <xolilesizephe.xs@gmail.com>
2023-10-15 15:51:58 +0200

    Complete Console Choice logic and working

--- javascript/game.js
+++ javascript/game.js
@@ -3,12 +3,30 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const gameResult = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let compChoice
 
 possibleChoices.forEach(possibleSelection => possibleSelection.addEventListener('click', (e)=>{
     userChoice =e.target.id 
     userChoiceDisplay.innerHTML = userChoice 
+
+    generateComputerChoice()
 }))
 
-function generateComputerCoice(){
-    const randomNumber = Math.random() * possibleChoices.length
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+    switch(randomNumber){
+        case 0:
+            compChoice = 'Rock'
+            break;
+        case 1:
+            compChoice ='Paper'
+            break;
+        case 2:
+            compChoice = 'Siccors'
+        
+
+    }
+
+    compChoiceDisplay.innerHTML = compChoice
 }
