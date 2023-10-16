c958e6c813da8c2573dd5795c987e069ae7d80dd
LostNativ <xolilesizephe.xs@gmail.com>
2023-10-16 09:19:37 +0200

    Result logic switch implement not reflecting

--- javascript/game.js
+++ javascript/game.js
@@ -1,15 +1,17 @@
 const compChoiceDisplay = document.getElementById('comp-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
-const gameResult = document.getElementById('result')
+const gameResultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let compChoice
+let result
 
 possibleChoices.forEach(possibleSelection => possibleSelection.addEventListener('click', (e)=>{
     userChoice =e.target.id 
     userChoiceDisplay.innerHTML = userChoice 
 
     generateComputerChoice()
+    getResult();
 }))
 
 function generateComputerChoice(){
@@ -26,7 +28,35 @@ function generateComputerChoice(){
             compChoice = 'Siccors'
         
 
+
+
+
+
+            
+
     }
 
     compChoiceDisplay.innerHTML = compChoice
+}
+
+function getResult(){
+
+    switch(compChoice = userChoice){
+        case 'PaperRock':
+        case 'RockSiccors':
+        case 'SiccorsPaper':
+            result = 'You Lose'
+            break;
+        case 'RockPaper':
+        case 'SiccorsRock':
+        case 'PaperSiccors':
+            result = 'You Win'
+            break;
+        case compChoice === userChoice:
+            result ='Draw'
+
+        }
+
+        gameResultDisplay,innerHTML =result
+
 }
